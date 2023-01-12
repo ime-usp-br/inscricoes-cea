06281d165efef30264439b532d673bb5a85b95fa
Ayrton Douglas Freitas Guimarães <ayrtondouglas@usp.br>
2023-01-12 10:53:49 -0300

    fix: foram feitos alguns ajustes para colocar em produção

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (env('FORCE_HTTPS') === true) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
@@ -23,6 +27,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+
+        if (env('FORCE_HTTPS', false)){
+            URL::forceScheme('https');
+        }    
     }
 }
