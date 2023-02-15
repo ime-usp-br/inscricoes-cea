656c67a9ad2413c624a274703af696294b2e59aa
Ayrton Douglas Freitas Guimarães <ayrtondouglas@usp.br>
2023-02-15 13:24:46 -0300

    fix: foi criada uma função para limpar os campos no latex

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -5,7 +5,11 @@ use \ForceUTF8\Encoding;
 if (! function_exists('clear_string')) {
     function clear_string($string)
     {
-        $string = Encoding::fixUTF8($string);
+        $string = Encoding::fixUTF8($string);        
+        $string = str_replace("\r\n","\n",$string);
+        $string = str_replace("\n\n","\n",$string);
+        $string = str_replace("\n","\n\n",$string);
+
         $badChars = ["&","%","$","#","_","{","}"];
         foreach($badChars as $c){
             $string = str_replace($c, "\\".$c, $string); 
